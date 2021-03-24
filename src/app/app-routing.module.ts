eb62b25840e9c9a9f076395d7dcb192fa40c12a8
Ankur Rastogi <ankur.rastogi01@nagarro.com>
2021-03-24 15:39:54 +0530

    added home page components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './home/page-not-found/page-not-found.component';
+import { ShellComponent } from './home/shell/shell.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    ]
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
